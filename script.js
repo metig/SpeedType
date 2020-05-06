42a48324ca95b493b165d9ba0c070c09d3ec582b
meti <49797027+metig@users.noreply.github.com>
2020-05-05 20:10:44 -0700

    Timer change, error check, measure speed

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+const TypeError = document.querySelector(".error")
+
 
 var timer =[0,0,0,0];
 var interval;
 var timerRunning = false;
 var countRed =0;
+var mySound;
+
+
 
 //Add leading zero to numbers 9 or below
  function leadingZero(time){
@@ -21,16 +26,17 @@ var countRed =0;
 function runTimer(){
   let minuteTimer = leadingZero(timer[0]);
   let secondTimer = leadingZero(timer[1]);
-//   let miliSecondTimer = leadingZero(timer[2]);
-  let currentTime = minuteTimer + ":" + secondTimer 
+  let milliSecondTimer = leadingZero(timer[2]);
+  var currentTime = minuteTimer + ":" + secondTimer ;
   theTimer.innerHTML = currentTime;
   timer[3]++;
 
 
   timer[0]= Math.floor((timer[3]/100)/60);
   timer[1]= Math.floor((timer[3]/100) - (timer[0] * 60));
-//   timer[2]= Math.floor(timer[3] - (timer[1] * 100)  - (timer[0] * 6000));
+  timer[2]= Math.floor(timer[3] - (timer[1] * 100)  - (timer[0] * 6000));
 
+  
 }
 
 // Match the text entered with the provided text
@@ -38,21 +44,23 @@ function spellCheck(){
     let textEntered = testArea.value;
     let originTextMatch = originText.substring(0, textEntered.length);
     
+
     // all the texts entered matched the origin text 
-    if (textEntered == originText){
-        clearInterval(interval);
-        testArea.style.borderColor = "#00ff62";
-       
-        // checking for shorter string
-    } else{
+     if (textEntered == originText){
+            clearInterval(interval);
+            
+            textEntered.style.borderColor = "#00ff62";
+      }
+
+     else{
         if(textEntered == originTextMatch){
-            testArea.style.borderColor = "#7feeee"
+            testArea.style.color = "green"
             
         }else{
-            testArea.style.borderColor = "red"
+            testArea.style.color = "red"
             countRed++
             return countRed;
-            
+            TypeError.innerHTML= countRed;
         }
     }
    
@@ -66,25 +74,73 @@ function start(){
         timerRunning = true;
         interval = setInterval(runTimer, 10);
     }
-    // console.log(textEnteredLength)
+    
 }
+//add sound to keypress
+  function sound(){
+      mySound = new Audio();
+      mySound.src = "iphone2.mp3";
+      let Mytext = testArea.value.length;
+      for (i=-1; i< Mytext; i++){
+          if(Mytext >= 0){
+              mySound.play();
+              
+          }
+      }
+  }
 
+  //calculate speed
+  function speedCheck(){
+    
+   
+      
+  }
+  
 
 
 //reset everything
 function reset(){
+    
     clearInterval(interval);
     interval = null;
-    timer =[0,0,0,0];
+    timer = [0,0,0,0];
     timerRunning = false;
 
+    let currTime = theTimer.innerHTML;
+    speed = currTime.replace(":", "")
+     let totalTime = speed[0]+ speed[1]; 
+      
+      let wordcount = testArea.value.length;
+
+      if ( totalTime <= -9 ){
+          alert("Opps! You need to type more words inorder to calculate your speed!")
+      
+        }else {
+           let grossWPM, netWPM , accuracy 
+           grossWPM = Math.floor((wordcount / 5) / totalTime)
+          
+           //calculate accuracy
+            let correctWords = wordcount -countRed
+            accuracy = Math.floor((correctWords/ wordcount) * 100)
+           if(grossWPM > countRed){
+            netWPM = Math.floor(grossWPM - (countRed / totalTime))
+           }else { netWPM = 0 }
+          TypeError.innerHTML ="Your typing speed is: "+ grossWPM 
+                        +"wpm. Your total errors:" + countRed 
+                        + " Your true speed: "+ netWPM 
+                        + " Your Accuracy:" + accuracy +"%"; 
+
+      }
+      
     testArea.value = "";
-    theTimer.innerHTML="00:00:00"
-    testArea.style.borderColor = "grey"
-    alert("your typing speed is 20wpm")
+    theTimer.innerHTML="00:00:00";
+
+    testArea.style.borderColor = "grey";
 }
 
 // Event listners for keyboard input and reset button
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
 resetButton.addEventListener("click", reset, false);
+testArea.addEventListener( "keydown", sound, false);
+//resetButton.addEventListener("click", speedCheck, false);
